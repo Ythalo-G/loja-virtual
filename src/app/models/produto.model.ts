76172a5d955e63b50bc4be58914ad7afe22c239a
Ythalo-G <galvaniythalo@gmail.com>
2021-08-25 17:54:36 -0300

    Tela inicial.

---
+++ src/app/models/produto.model.ts
@@ -0,0 +1,10 @@
+export class Produto {
+    constructor(
+        public id: number,
+        public nome: string,
+        public preco: number,
+        public fotos: string[],
+        public descricao: string[] 
+    ){
+    }
+}
