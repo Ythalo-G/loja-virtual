26c1b63d29606aae69daf755d0bb95a789a39bf4
Ythalo-G <galvaniythalo@gmail.com>
2021-08-27 17:49:29 -0300

    Carrinho e finalizar compra

--- src/app/componentes/carrinho/carrinho.component.ts
+++ src/app/componentes/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/models/produto.model';
 import { CarrinhoService } from 'src/app/servicos/carrinho.service';
 
 @Component({
@@ -13,7 +14,13 @@ export class CarrinhoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  finalizarCompra(){}
+  removerCarrinho(id: number){
+    this.carrinhoService.remover(id);
+  
+  }
+  adicionarCarrinho(produto: Produto){
+    this.carrinhoService.adicionar(produto);
+  }
 
   get itens(){
     return this.carrinhoService.itens;
